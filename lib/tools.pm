b53d3ff0841085ea73b0ba5ef47e82e1380ef4dd
EclipseUser <EclipseUser@mail.rf>
2023-02-09 12:35:58 +0300

    add 3 sub in tools.pm

---
+++ lib/tools.pm
@@ -0,0 +1,87 @@
+package tools;
+use strict;
+use warnings FATAL => 'all';
+
+sub read_conf {
+    my ($file_name) = @_;
+    if (!open CON_FILE, '<', $file_name) {
+        die "File \"$file_name\" not open. Error $!";
+    }
+
+    my %user_pass;
+    while (my $line = <CON_FILE>) {
+        chomp($line);
+        $line =~ s/\s//g;
+        if ($line =~ /^#/) {
+            next;
+        }
+        if (!($line =~ /.+=.+/)) {
+            next;
+        }
+        my ($key_conf, $val_conf) = split '=', $line;
+        $user_pass{$key_conf} = $val_conf;
+    }
+    close CON_FILE;
+    return %user_pass;
+}
+
+sub read_users {
+    my ($file_name) = @_;
+    if (!open USERS_FILE, '<', $file_name) {
+        exit;
+    }
+    my @users;
+    while (my $user = <USERS_FILE>) {
+        chomp($user);
+        $user =~ s/\s//g;
+        push(@users, $user);
+    }
+    close USERS_FILE;
+    return @users;
+}
+
+sub add_user {
+    my($file_name, $new_user) = @_;
+    my @users = read_users($file_name);
+    my $is_exist = 0;
+    for my $cur_user (@users){
+        if($cur_user eq $new_user){
+            $is_exist = 1;
+            last;
+        }
+    }
+    if($is_exist){
+        return;
+    }
+    if (!open USERS_FILE, '>>', $file_name) {
+        exit;
+    }
+    print USERS_FILE  "$new_user\n" ;
+    close USER_FILE;
+}
+
+sub remove_user{
+    my($file_name, $rem_user) = @_;
+    my @users = read_users($file_name, $rem_user);
+    my @new_user;
+    my $is_exist = 0;
+    for(my $i =0; $i < @users; $i++){
+        if(!($users[$i] eq $rem_user)){
+            push @new_user, $users[$i];
+        } else{
+            $is_exist = 1;
+        }
+    }
+    print @users;
+    if(!$is_exist){
+        return;
+    }
+    if (!open USERS_FILE, '>',  $file_name) {
+        exit;
+    }
+    for my $cur_user (@new_user){
+        print USERS_FILE  "$cur_user\n" ;
+    }
+    close USER_FILE;
+}
+1;
--- rt2_1.pl
+++ rt2_1.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
+use tools;
 
-my %result_hash = tools::read_conf ('ConfigTest');
-print %result_hash;
+# my @result = tools::read_users ('users');
+# print @result;
+tools::remove_user('users', 'super');
