fc0c74ab0d7cd7cd8cccffd9903ba1d0cfabd84c
EclipseUser <EclipseUser@mail.rf>
2023-02-10 11:23:25 +0300

    add test file userspass. New format of file. change tools::get. replace rt2_2. add rt3_1.pl.

--- lib/tools.pm
+++ lib/tools.pm
@@ -2,29 +2,6 @@ package tools;
 use strict;
 use warnings FATAL => 'all';
 
-sub read_conf {
-    my ($file_name) = @_;
-    if (!open CON_FILE, '<', $file_name) {
-        die "File \"$file_name\" not open. Error $!";
-    }
-
-    my %user_pass;
-    while (my $line = <CON_FILE>) {
-        chomp($line);
-        $line =~ s/\s//g;
-        if ($line =~ /^#/) {
-            next;
-        }
-        if (!($line =~ /.+=.+/)) {
-            next;
-        }
-        my ($key_conf, $val_conf) = split '=', $line;
-        $user_pass{$key_conf} = $val_conf;
-    }
-    close CON_FILE;
-    return %user_pass;
-}
-
 sub read_users {
     my ($file_name) = @_;
     if (!open USERS_FILE, '<', $file_name) {
@@ -32,10 +9,13 @@ sub read_users {
         return;
     }
     my @users;
-    while (my $user = <USERS_FILE>) {
-        chomp($user);
-        $user =~ s/\s//g;
-        push(@users, $user);
+    while (my $line = <USERS_FILE>) {
+        chomp($line);
+        if (!($line =~ /.+=.+/)) {
+            next;
+        }
+        $line =~ m/^(\w+)=/;
+        push @users, $1;
     }
     close USERS_FILE;
     return @users;
---
+++ rt3_1.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+use tools;
+
+my $users_file_name = 'userspass';
+my $user_name = $ENV{'user_name'};
+my $action = $ENV{'action'};
+
+if(!defined($action)){
+    print "action $action is null\n";
+    print "action $action is null\n";
+    exit;
+}
+if($action eq 'get'){
+    my @users = tools::read_users($users_file_name);
+    print "Users:\n";
+    for my $cur_user (@users){
+        print "User[$cur_user]\n";
+    }
+}elsif($action eq 'add'){
+    if(!defined($user_name)){
+        print "Username is null\n";
+        exit;
+    }
+    my $result_adding = tools::add_user($users_file_name, $user_name);
+    if($result_adding){
+        print "Add user[$user_name].\n";
+    }else{
+        print "User [$user_name] not added.\n";
+    }
+}elsif($action eq 'del'){
+    if(!defined($user_name)){
+        print "Username is null\n";
+        exit;
+    }
+    my $result_deleting = tools::remove_user($users_file_name, $user_name);
+    if($result_deleting){
+        print "Del user[$user_name].\n";
+    }else{
+        print "User [$user_name] not deleted.\n";
+    }
+}else{
+    print "Action [$action] is unknown.\n";
+}
