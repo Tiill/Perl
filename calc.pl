fb5f69843c14d7ee4347ab7b79df442d5954eb33
EclipseUser <EclipseUser@mail.rf>
2023-02-15 14:55:27 +0300

    Calc fix

--- calc.pl
+++ calc.pl
@@ -5,33 +5,46 @@ use warnings FATAL => 'all';
 my $result;
 my $operation;
 my $arg_2;
+my $history = '';
 print "First argument: ";
 while (1) {
     my $line = <STDIN>;
     chomp($line);
-    if($line =~ /=/){
-        exit;
+    if ($line =~ /=/) {
+        print "\033[2J";
+        print "\033[1; 0H";
+        print $history . "=$result" . "\n";
+        $result = undef;
+        print "First argument: ";
+        next;
     }
-    if(!defined($result)) {
-        if (!($line =~ /\d+/)) {
-            print 'arg bad\n';
+    if (!defined($result)) {
+        if (!($line =~ /^\d+$/)) {
+            print "arg bad\n";
             next;
         }
         $result = $line;
+        $history = $history . $result;
         print "Operation: ";
-    }elsif(!defined($operation)){
-        if (!($line =~ /[+\-*\/]/)) {
-            print 'oper bad\n';
+    }
+    elsif (!defined($operation)) {
+        if (!($line =~ /^[+\-*\/]{1,1}$/)) {
+            print "oper bad\n";
             next;
         }
         $operation = $line;
+        $history = $history . $operation;
+        if ($operation eq '/' or $operation eq '*') {
+            $history = '(' . $history . ')';
+        }
         print "Second argument: ";
-    }elsif(!defined($arg_2)){
-        if (!($line =~ /\d+/)) {
-            print 'arg2 bad\n';
+    }
+    elsif (!defined($arg_2)) {
+        if (!($line =~ /^\d+$/)) {
+            print "arg2 bad\n";
             next;
         }
-        if(($operation =~ /\//) and ($line == 0)){
+        if (($operation =~ /\//) and ($line == 0)) {
             print "Divide by zero!\n";
             print "First argument: $result\n";
             print "Second argument: ";
@@ -39,7 +52,8 @@ while (1) {
             next;
         }
         $arg_2 = $line;
-        $result = calculate($result, $operation, $arg_2);
+        $history = $history . $arg_2;
+        $result = _calculate($result, $operation, $arg_2);
         $operation = undef;
         $arg_2 = undef;
         print "Result: $result\n";
@@ -47,7 +61,7 @@ while (1) {
     }
 }
 
-sub calculate {
+sub _calculate {
     my ($arg_1, $operation, $arg_2) = @_;
     if ($operation eq '+') {
         return $arg_1 + $arg_2;
