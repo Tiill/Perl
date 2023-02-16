90bda99b8090d3b7d3b4c2c4288bac49373762d3
EclipseUser <EclipseUser@mail.rf>
2023-02-16 17:16:02 +0300

    snail rename check sub

--- Snail_check.pl
+++ Snail_check.pl
@@ -7,11 +7,11 @@ my $debt = 187;
 my $snail = 8;
 my $sprint = 50;
 
-my $result = ceil(check($debt, $snail, $sprint));
+my $result = ceil(_check($debt, $snail, $sprint));
 my $result_string = ($result > 0) ? "Справится за $result спринтов" : "Не справится.";
 print $result_string;
 
-sub check {
+sub _check {
     my ($debt, $snail_vel, $tasks_in_sprint) = @_;
     return $debt / ((($snail_vel * 10) - $tasks_in_sprint) - 0.0000001);
 }
