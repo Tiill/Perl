5e82155eb232741d89be0b8592709bdbb4074639
EclipseUser <EclipseUser@mail.rf>
2023-02-17 10:36:14 +0300

    add _get_file_lines

--- Scriptina.pl
+++ Scriptina.pl
@@ -6,20 +6,14 @@ my $target_file_name = 'newsletter';
 my $target_file_path = './newsletters/';
 my $ban_file_name = 'ban_list';
 my $ban_file_path = './etc/';
-my @ban_array = _get_ban_list($ban_file_name, $ban_file_path);
+my @ban_array = _get_lines_file($ban_file_name, $ban_file_path);
 my $minimal_count = 2;
 my $input_text = '';
 my %all_words;
 my $words_count;
 
-if (!open TARGET_FILE, '<', $target_file_path . $target_file_name) {
-    print "File $target_file_path$target_file_name not exist!\n";
-    exit;
-}
-while (my $line = <TARGET_FILE>) {
-    chomp($line);
-    $input_text = $input_text . $line;
-}
+my @input_lines = _get_lines_file($target_file_name, $target_file_path);
+$input_text = join('', @input_lines);
 my @words_array = split(/[\s.,!?:"]+/, $input_text);
 foreach my $cur_word (@words_array) {
     if (!($cur_word =~ /\w+/)) {
@@ -44,7 +38,6 @@ for my $cur_word (sort {$all_words{$b} <=> $all_words{$a}} keys %all_words) {
     print "$cur_word=$all_words{$cur_word}\n";
 }
 print "All words count: $words_count.\n";
-close TARGET_FILE;
 
 sub _check_word {
     my $word = $_[0];
@@ -56,17 +49,17 @@ sub _check_word {
     return 1;
 }
 
-sub _get_ban_list {
-    my ($ban_file_name, $ban_file_path) = @_;
-    my @ban_list;
-    if (!open BAN_FILE, '<', $ban_file_path . $ban_file_name) {
-        print "Ban file not exist $ban_file_path$ban_file_name .\n";
+sub _get_lines_file {
+    my ($file_name, $file_path) = @_;
+    my @lines;
+    if (!open IN_FILE, '<', $file_path . $file_name) {
+        print "File not exist $file_path$file_name .\n";
         return;
     }
-    while (my $line = <BAN_FILE>) {
-        chomp $line;
-        push @ban_list, $line;
+    while (my $line = <IN_FILE>) {
+        chomp($line);
+        push(@lines, $line);
     }
-    close BAN_FILE;
-    return @ban_list;
+    close IN_FILE;
+    return @lines;
 }
